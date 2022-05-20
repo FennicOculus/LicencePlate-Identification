a2ab79700d7dc799ffbecb829e0402ea63f1efdc
Abd Samy Aghiles <78227576+FennicOculus@users.noreply.github.com>
2022-05-20 20:17:23 +0100

    First Commit

--- PlateDetection.py
+++ PlateDetection.py
@@ -1,57 +1,54 @@
-
-import cv2 as cv
-import glob
+import cv2
+from Functions import NameGeneration
+from tkinter import filedialog, messagebox
+from tkinter import *
+import os
 
 ''' Dataset Kaggle https://www.kaggle.com/datasets/andrewmvd/car-plate-detection'''
+'''https://www.section.io/engineering-education/license-plate-detection-and-recognition-using-opencv-and-pytesseract/'''
 
-sift = cv.SIFT_create()
+def Seg():
 
-# model = cv.imread('queries/matricule4.png',0)
-# cv.imshow('image model',model)
-# cv.waitKey(0)
-# requet = cv.imread('License Plates/bbcac63e32bd8137_jpg.rf.ef4704b0ada4fbbf613143abf52f6f86.jpg',0)
-# cv.imshow('image requet',requet)
-# cv.waitKey(0)
-model = [cv.imread(file) for file in glob.glob("C:/Users/Lina/Desktop/Sam/Progs/ProjetCV/queries/*.png")]
-requet = [cv.imread(file) for file in glob.glob("C:/Users/Lina/Desktop/Sam/Progs/ProjetCV/License Plates/*.jpg")]
+    root = Tk()
+    root.withdraw()
+    files = filedialog.askopenfilenames(initialdir='New Cars/', title='Ajout d\'une voiture')
+    if not files:
+        root.destroy()
+        return
+    #showing the original Image
+    image = cv2.imread(str(files[0]))
 
+    #Converting it to  gray Scale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def SiftDetect(img):
-    kp, des = sift.detectAndCompute(img, None)
-    result = cv.drawKeypoints(img, kp, des)
-    return result, kp, des
+    #Reducing the noise
+    gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
 
-i = 0
-for mod in model:
-    i += 1
-    j = 0
-    img1, kp1, des1 = SiftDetect(mod)
-    for req in requet:
-        j += 1
-        img2, kp2, des2 = SiftDetect(req)
+    edged = cv2.Canny(gray_image, 200, 300)
 
-        bf = cv.BFMatcher(cv.NORM_L2, crossCheck=False)
-        Matches = bf.knnMatch(des2, des1, k=2)
-        good = []
-        for m, n in Matches:
-            if m.distance < 0.3*n.distance:
-                good.append([m])
-        imgf = cv.drawMatchesKnn(req, kp2, mod, kp1, good, None, flags=2)
-        if good != []:
-            print("matricule "+str(i)+" voiture "+str(j)+" "+str(good))
-            cv.imshow('image Final', imgf)
-            cv.waitKey(1000)
-    cv.destroyAllWindows()
+    cnts,_ = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    image1=image.copy()
+    cv2.drawContours(image1,cnts,-1,(0,255,0),3)
 
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True) [:30]
+    screenCnt = None
+    image2 = image.copy()
+    cv2.drawContours(image2,cnts,-1,(0,255,0),3)
+    name = NameGeneration('DataBase/*.png')
 
-
-
-
-
-
-
-
-
-
-
-
+    for c in cnts:
+        perimeter = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.018 * perimeter, True)
+        if len(approx) == 4: 
+            x,y,w,h = cv2.boundingRect(c) 
+            new_img=image[y:y+h,x:x+w]
+            cv2.imshow("test", new_img)
+            cv2.waitKey(0)
+            MsgBox = messagebox.askquestion ('Save','Do you want to save this image ?',icon = 'warning')
+            if MsgBox == 'yes':
+                cv2.imwrite('DataBase/'+str(name)+'.png',new_img)
+                os.remove(files[0])
+            else:
+                messagebox.showinfo('Return','Image hasn\'t been saved')
+            break
+    root.destroy()
