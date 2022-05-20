e162602bdadf8462812c13fbaa5cd8d99e74c910
Abd Samy Aghiles <78227576+FennicOculus@users.noreply.github.com>
2022-05-20 20:18:41 +0100

    Added 4 Buttons, bug fixes

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 from OneCar import reco
+from PlateDetection import Seg
 from PlateExtraction import addCar
 
-
-    
 win = tk.Tk()
 win.title('Parking Plate Identification')
-win.geometry("640x480")
+win.geometry("760x560")
 win.resizable(0,0)
 
 frame = tk.Frame(win, width=600, height=400)
 frame.pack()
 frame.place(anchor='center', relx=0.5, rely=0.5)
 img = Image.open("Interface/BackGround.jpg")
-img = ImageTk.PhotoImage(img.resize((640, 480)))
+img = ImageTk.PhotoImage(img.resize((770, 570)))
 
 label = tk.Label(frame, image = img)
 label.pack()
@@ -23,8 +22,14 @@ label.pack()
 button1 = tk.Button(win, text='Recognition From Image', width=25, command=reco)
 button1.pack(side='left', padx=50, pady=0)
 
-button2 = tk.Button(win, text='Add Car To Database', width=25, command=addCar)
+button2 = tk.Button(win, text='Manually Adding Car To Database', width=25, command=addCar)
 button2.pack(side='right', padx=50, pady=0)
+
+button3 = tk.Button(win, text='Ajout Automatique Matricule', width=25, command=Seg)
+button3.pack(side='top', padx=0, pady=150)
+
+button3 = tk.Button(win, text='Ouvrir Caméra', width=25)
+button3.pack(side='bottom', padx=0, pady=100)
 
 
 
